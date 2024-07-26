e1b72e24d4eb1eb35b26fe85fc14943af77e7237
mehmetNetAx <mehmetdemir@netaxtech.com>
2024-07-26 11:42:08 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,56 +1,45 @@
 import streamlit as st
-from openai import OpenAI
 
-# Show title and description.
-st.title("💬 Chatbot")
-st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+# ---- Pages
+about_page = st.Page(
+    page="views/about.py",
+    title="About",
+    icon=":material/account_circle:",
+    default=True,
+)
+chatbot_page= st.Page(
+    page="views/chatbot.py",
+    title="Chat wit Contracts",
+    icon=":material/smart_toy:"
+)
+contract_page= st.Page(
+    page="views/contract_create.py",
+    title="Create Contract",
+    icon=":material/smart_toy:"
 )
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
+template_page= st.Page(
+    page="views/contract_fill.py",
+    title="Create Contract from Template",
+    icon=":material/smart_toy:"
+)
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+dashboard_page= st.Page(
+    page="views/service_dashboard.py",
+    icon=":material/bar_chart:",
+)
 
-    # Create a session state variable to store the chat messages. This ensures that the
-    # messages persist across reruns.
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+# Page Navigation
+# pg = st.navigation(pages=[about_page, chatbot_page, dashboard_page])
+pg = st.navigation(
+    {
+    "info" : [about_page],
+    "Services": [contract_page,template_page,chatbot_page, dashboard_page]
+    }
+)
 
-    # Display the existing chat messages via `st.chat_message`.
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
-    # Create a chat input field to allow the user to enter a message. This will display
-    # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
-
-        # Store and display the current prompt.
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
-
-        # Generate a response using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
-
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
-        with st.chat_message("assistant"):
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+# Shared on all pages
+st.logo("assets/papir_logo.png")
+st.sidebar.text("Papir.ai Services")
+# Run Navigation
+pg.run()
